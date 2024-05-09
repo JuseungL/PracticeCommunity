bee458d473771237608b625d34c450ec601613ce
juseung <juseung0619@gmail.com>
2024-05-09 14:08:27 +0900

    first commit

---
+++ src/main/java/com/server/InvestiMate/InvestiMateApplication.java
@@ -0,0 +1,13 @@
+package com.server.InvestiMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestiMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestiMateApplication.class, args);
+	}
+
+}
