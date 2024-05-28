e49f1a5850b00608bc13981a1f68d5df7938e495
juseung <juseung0619@gmail.com>
2024-05-28 15:48:26 +0900

    starter

--- src/main/java/com/server/InvestiMate/api/chat/repository/ChatMessageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.server.InvestiMate.api.chat.repository;
-
-import com.server.InvestiMate.api.chat.domain.ChatMessage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
-}
--- src/main/java/com/server/InvestiMate/api/chat/repository/ChatSessionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.server.InvestiMate.api.chat.repository;
-
-import com.server.InvestiMate.api.chat.domain.ChatMessage;
-import com.server.InvestiMate.api.chat.domain.ChatSession;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatSessionRepository extends JpaRepository<ChatSession, Long> {
-}
